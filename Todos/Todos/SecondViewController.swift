ab03ec58e6f8e1c192b892024e0b02816296aa91
Tim Wheeler <tim@timwheeler.com>
2019-06-17 23:11:43 -0400

    working on todos app

--- Todos/Todos/SecondViewController.swift
+++ Todos/Todos/SecondViewController.swift
@@ -8,15 +8,53 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITableViewDataSource,
+	UITableViewDelegate {
+	
+	var todos = [String]()
+	
+	@IBOutlet weak var table: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-	
 		
-	
+		let todosObject = UserDefaults.standard.object(forKey: "todos")
 		
+		todos = (todosObject as? Array<String>)!
+		
+		if todos.count > 0 {
+
+			print(todos)
+
+		}
+		
+		for todo in todos {
+			
+			print(todo)
+			
+		}
+		
+		
+		table.reloadData()
 	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return todos.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+		
+			cell.textLabel?.text = todos[indexPath.row]
+		
+			print("todos[indexPath.row]", todos[indexPath.row])
+		
+		return cell
+	}
+	
+
+	
 
 
 }
